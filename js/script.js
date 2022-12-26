6ec9aa0feab90831aa1d7a6d45ebfdefa4190771
savik7 <viktor.savchenko00@gmail.com>
2022-12-26 20:31:53 +0300

    Основы JavaScript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let userName = "Alex"
+alert(userName)
+console.log(userName)
+console.log(123)
+
+let user = {
+  name: "Иван",
+  lastName: "Иванович",
+  age: 25
+};
+
+alert(user.name);
+alert(user.lastName);
+alert(user.age);
